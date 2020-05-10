7efc4029ddb2bae98b034d9ea3472b92b29abedb
bagusnata330 <bagusnata330@gmail.com>
2020-05-10 20:36:52 +0800

    change price again

--- src/MarketComponent/MainContent.js
+++ src/MarketComponent/MainContent.js
@@ -64,7 +64,7 @@ class MainContent extends React.Component {
       {
         id: 8,
         name: "Jordan 1 Retro High Pine green",
-        price: 198,
+        price: 197,
         picture:
           "https://stockx.imgix.net/Air-Jordan-1-Retro-High-Pine-Green-Black-Product.jpg",
       },
