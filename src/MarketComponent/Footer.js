6ac19b0259b3023f97184e9cb05dc6a0bf224f0f
bagusnata330 <bagusnata330@gmail.com>
2020-05-24 14:01:35 +0800

    nyoba form di footer

--- src/MarketComponent/Footer.js
+++ src/MarketComponent/Footer.js
@@ -1,14 +1,82 @@
-import React from "react"
+import React, { useState } from "react";
+import { produce } from "immer";
+import { generate } from "shortid";
 
-function Footer(){
+const Footer = () => {
+  const [shoe, setShoe] = useState([
+    {
+      id: "1",
+      shoeName: "jordan 1 high",
+      shoePrice: "500",
+      shoePicture:
+        "https://stockx.imgix.net/Air-Jordan-1-Retro-High-Court-Purple-White_01.jpg"
+    },
+  ]);
+
   return (
-    <footer>
-      <hr/>
-      {/* <h1>FOOTER</h1> */}
-    </footer>
-    
-  )
-}
+    <div style={{ textAlign: "center" }}>
+      <button
+        onClick={() => {
+          setShoe((currentShoe) => [
+            ...currentShoe,
+            {
+              id: generate(),
+              shoeName: "",
+              shoePrice: "",
+              shoePicture: ""
+            },
+          ]);
+        }}
+      >
+        add new shoe
+      </button>
+      {shoe.map((s, index) => {
+        return (
+          <div key={s.id}>
+            <input
+              onChange={(e) => {
+                const shoeName = e.target.value;
+                setShoe((currentShoe) =>
+                  produce(currentShoe, (v) => {
+                    v[index].shoeName = shoeName;
+                  })
+                );
+              }}
+              value={s.shoeName}
+              placeholder="shoe name"
+            />
 
+            <input
+              onChange={(e) => {
+                const shoePrice = e.target.value;
+                setShoe((currentShoe) =>
+                  produce(currentShoe, (v) => {
+                    v[index].shoePrice = shoePrice;
+                  })
+                );
+              }}
+              value={s.shoePrice}
+              placeholder="shoe price"
+            />
 
-export default Footer
+            <input
+              onChange={(e) => {
+                const shoePicture = e.target.value;
+                setShoe((currentShoe) =>
+                  produce(currentShoe, (v) => {
+                    v[index].shoePicture = shoePicture;
+                  })
+                );
+              }}
+              value={s.shoePicture}
+              placeholder="shoe picture"
+            />
+          </div>
+        );
+      })}
+      <div>{JSON.stringify(shoe)}</div> <br/><br/>
+    </div>
+  );
+};
+
+export default Footer;
