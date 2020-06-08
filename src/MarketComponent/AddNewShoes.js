eb4fd38b909975b8deff4caff71f1c8902eeb173
bagusnata330 <bagusnata330@gmail.com>
2020-06-08 20:13:11 +0800

    .

--- src/MarketComponent/AddNewShoes.js
+++ src/MarketComponent/AddNewShoes.js
@@ -6,7 +6,7 @@ import { generate } from "shortid";
 const AddNewShoes = () => {
   const [shoe, setShoe] = useState([
     {
-      id: "",
+      id: generate(),
       shoeName: "",
       shoePrice: "",
       shoePicture: "",
@@ -115,7 +115,7 @@ const AddNewShoes = () => {
         </div>
 
         {/* <div>{shoe ? shoecard : null}</div> */}
-        <div>{JSON.stringify(shoe)}</div> <br />
+        {/* <div>{JSON.stringify(shoe)}</div> <br /> */}
         {/* <br /> */}
       </div>
     </div>
